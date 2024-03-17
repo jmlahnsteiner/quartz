ed7f0c0d8b82a0a0dd5bab3ff4d5db8b2aaba0b1
Johannes Lahnsteiner <51122768+jmlahnsteiner@users.noreply.github.com>
2024-03-17 14:29:32 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
-      cdnCaching: false,
+      cdnCaching: true,
       typography: {
         header: "Langar",
         // Poor Story
