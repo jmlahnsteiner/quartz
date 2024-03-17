bf787cfd4726c4a2ed28a2826b96393440de4cc8
Johannes Lahnsteiner <51122768+jmlahnsteiner@users.noreply.github.com>
2024-03-17 15:40:13 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,15 +36,15 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
-export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [],
-}
+// // components for pages that display lists of pages  (e.g. tags or folders)
+// export const defaultListPageLayout: PageLayout = {
+//   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+//   left: [
+//     Component.PageTitle(),
+//     Component.MobileOnly(Component.Spacer()),
+//     Component.Search(),
+//     Component.Darkmode(),
+//     Component.DesktopOnly(Component.Explorer()),
+//   ],
+//   right: [],
+// }
